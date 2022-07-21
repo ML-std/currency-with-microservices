fec41490ecdd84cfe4eb3abf52fa2a9513018248
ML-std <ML-std@github. com>
2022-07-21 22:35:34 +0300

    H2 database initialized, script data is created.

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into CURRENCY_EXCHANGE(id,currency_from,currency_to,conversion_multiple,environment) values(10004,'USD','TR', 20,8001)
+insert into CURRENCY_EXCHANGE(id,currency_from,currency_to,conversion_multiple,environment) values(10002,'AUD','TR', 17,8001)
+insert into CURRENCY_EXCHANGE(id,currency_from,currency_to,conversion_multiple,environment) values(10006,'EUR','TR',  19,8001)
