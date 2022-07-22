45cc27863d2c4aaf51fdb40dd5444458e96bc714
ML-std <ML-std@github. com>
2022-07-22 19:18:10 +0300

    Proxy finally added

--- conversion-service/src/main/java/com/currencymicroservices/conversionservice/proxy/CurrencyConversionProxy.java
+++ conversion-service/src/main/java/com/currencymicroservices/conversionservice/proxy/CurrencyConversionProxy.java
@@ -5,8 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient
+@FeignClient(name = "currency-exchange")
 public interface CurrencyConversionProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
-    public CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to );
+     CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to );
 }
