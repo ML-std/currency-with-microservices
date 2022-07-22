907b07551f1da25d95df02b9bfebe7c9f5379ffc
ML-std <ML-std@github. com>
2022-07-22 12:08:23 +0300

    CurrencyConversion module is created, communication with CurrencyExchange is handled.

---
+++ currency-exchange-service/src/test/java/com/currencymicroservices/currencyexchangeservice/CurrencyExchangeServiceImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.currencymicroservices.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceImplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
